f4fe49c1d8c26543aa137a6d623efb0560b7a666
Husain Jinia <husain.jinia0201@gmail.com>
2022-07-16 22:32:07 +0530

    changes

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import TopHeader from './topHeader'
-import BottomHeader from './bottomHeader'
+import TopHeader from './TopHeader'
+import bottomHeader from './BottomHeader'
 
 const Header = () => {
   return (
     <div className='mx-3 xl:mx-20 lg:mx-20 md:mx-10'>
         <div className='flex flex-col bg-white-400'>
             <TopHeader/>
-            <BottomHeader/>
+            <bottomHeader/>
         </div>
     </div>
   )
